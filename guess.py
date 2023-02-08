5c81db29960a6b62d32e835f32c2dd36a366636c
Joa_sss <quabotgithub@gmail.com>
2023-02-08 10:27:10 +0100

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,9 +1,20 @@
+########################################
+# Naam: Joas Bruil
+# Doel: nummer randen van de computer
+# Klas: h4e
+# Docent: JBU
+# Project: Number Guessing Game
+# Modules: random
+########################################
+
 import random
 
+# Om te kunnen restarten nadat de game is afgelopen.
 while True:
     print("____________________________________");
     print();
 
+    # Krijg de input van de user en verander het naar integers
     rawLowestNumber = input("Please enter the lowest number that you want to me to guess a number between: ");
     rawHighestNumber = input("Please enter the highest number that you want to me to guess a number between: ");
     rawGuesses = input("Please enter the amount of guesses you want to do: ");
@@ -14,8 +25,10 @@ while True:
     madeGuesses = int(0);
 
 
+    # Check of het geldige getallen zijn
     if isinstance(lowestNumber, int) and isinstance(highestNumber, int):
 
+        # Failsaves (crashes voorkomen en eisen)
         if lowestNumber > highestNumber:
             print("The lowest number cannot be higher than the highest number.");
             break;
@@ -37,6 +50,7 @@ while True:
             print("The amount of guesses cannot be higher than 8.");
             break;
 
+        # Start het spel (berichten en random getallen genereren)
         print();
         print("I am going to guess a number between " + str(lowestNumber) + " and " + str(highestNumber) + "!");
         print();
@@ -51,6 +65,7 @@ while True:
 
         if guesses > total: guesses = total-1;
 
+        # Zolang het nummer niet geraden is, laat de gebruiker raden.
         while madeGuesses < guesses:
             madeGuesses + 1;
             guessRaw = input("Guess the number between " + str(lowestNumber + 1) + " and " + str(highestNumber + 1) + ": ");
@@ -60,6 +75,7 @@ while True:
 
             guess = int(guessRaw);
 
+            # Handle the input van de user
             if isinstance(guess, int):
                 if guess == rndInt:
                     print("Correct! The number was: " + str(rndInt) + "! You guessed it in " + str(madeGuesses) + "guesses.");
@@ -74,6 +90,7 @@ while True:
     else:
         print("You entered invalid numbers!");
 
+    # Laat het spel weer opnieuw beginnen
     again = input("Do you want to play again? (Y/N) ").lower();
 
     validAgain = ['y', 'yes', 'n', 'no'];
